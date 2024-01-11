1e7279e72be224f9ef8778dd104f3249fd130a79
imopbuilder <opbuilder9@gmail.com>
2024-01-11 14:51:03 +0530

    feat: made the letters to generate smoothly in mobile version

--- src/components/global/random-characters.tsx
+++ src/components/global/random-characters.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CHARS, INITIAL_LETTERS } from '@/constants/app';
-import { ReactNode, useRef, useState, type MouseEvent } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 
 const randomChar = () => CHARS[Math.floor(Math.random() * (CHARS.length - 1))];
 const randomString = (length: number) => Array.from(Array(length)).map(randomChar).join('');
@@ -11,12 +11,12 @@ export function RandomCharacters({ children }: { children: ReactNode }) {
 	const cardRef = useRef<HTMLDivElement>(null);
 	const lettersRef = useRef<HTMLDivElement>(null);
 
-	function handleMouseMove(e: MouseEvent<HTMLDivElement>) {
+	function handlePosition(clientX: number, clientY: number) {
 		if (!cardRef.current || !lettersRef.current) return;
 
 		const rect = cardRef.current.getBoundingClientRect();
-		const x = e.clientX - rect.left;
-		const y = e.clientY - rect.top;
+		const x = clientX - rect.left;
+		const y = clientY - rect.top;
 
 		lettersRef.current.style.setProperty('--x', `${x}px`);
 		lettersRef.current.style.setProperty('--y', `${y}px`);
@@ -28,7 +28,8 @@ export function RandomCharacters({ children }: { children: ReactNode }) {
 		<div
 			ref={cardRef}
 			className='group card flex items-center justify-center aspect-square relative rounded-3xl overflow-hidden cursor-pointer border'
-			onMouseMove={handleMouseMove}
+			onMouseMove={(e) => handlePosition(e.clientX, e.clientY)}
+			onTouchMove={(e) => handlePosition(e.touches[0].clientX, e.touches[0].clientY)}
 		>
 			<div className='card-image flex items-center justify-self-center relative z-[4]'>{children}</div>
 			<div className='card-gradient h-full w-full absolute pointer-events-none z-[3] mix-blend-darken' />
