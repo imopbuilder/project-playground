fbbfde7502496dc17f90ff2dade35bc9d862028c
imopbuilder <opbuilder9@gmail.com>
2024-01-11 14:49:59 +0530

    styles: change padding in the tech-stack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
 		<Fragment>
 			<Header />
 			<main>
-				<section className='min-h-hvh p-5'>
+				<section className='min-h-hvh p-2 md:p-5'>
 					<div>
 						<h2 className='scroll-m-20 text-2xl font-semibold tracking-tight pb-8'>Tech Stack</h2>
-						<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4'>
+						<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-4'>
 							<Stack />
 						</div>
 					</div>
